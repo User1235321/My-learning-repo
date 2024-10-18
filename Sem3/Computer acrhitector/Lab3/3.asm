6a93453cab2da1335173a2b30a10a2cc7d986bf2
User1235321 <pavreb1235321@gmail.com>
2024-10-18 11:13:45 +0300

    Finish third lab in CA

--- Sem3/Computer acrhitector/Lab3/3.asm
+++ Sem3/Computer acrhitector/Lab3/3.asm
@@ -1,42 +1,50 @@
 global _start
+numOfEl equ 5
 section .data
-  arr db 170, 171, 172, 173, 174, 175, 176, 177, 178, 179
-  arr2 db 10 dup(180)
+  arr dw 1, 20, 300, 4000, 50000
+  arr2 dw numOfEl dup(0)
 section .text
   print:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, toPrint
-    mov edx, 1
-    int 0x80
+    mov [toPrint], ax		; put ax in toPrint
+    mov eax, 4			; sys call for sys_write
+    mov ebx, 1			; file descriptor 1 (stdout)
+    mov ecx, toPrint		; pointer to the character to print
+    mov edx, 1			; length of the output
+    int 0x80; call kernel
     ret
 
-  prePrint:
-    mov bl, 10
-    div bl
-    mov [toPrint], ah
-    push ax
-    call print
-    pop ax
-    cmp ax, 0
-    jnz prePrint
+  printBigNum:
+    mov rcx, 0
+    decomposition:
+      mov bx, 10
+      mov dx, 0
+      div bx; ax = dx:ax / bx; dx = ax % bx
+      push dx
+      add rcx, 1; ++rcx
+      cmp ax, 0; (ax > 0) ? (ZF = 1) : (ZF = 0)
+      jnz decomposition; while (ZF != 1) call jmp decomposition
+    outputLoop:
+      pop ax
+      add ax, '0' ; char(9) is '\t', but char(9 + '0') is '9'
+      push rcx
+      call print
+      pop rcx
+      loop outputLoop
     ret
   printTemplate:
-    mov rcx, 10
+    mov rcx, numOfEl; number of elements in array
     fullPrint:
-      mov ax, [rbx]
-      add rbx, 1
+      mov ax, [rbx]; ax = *rbx
+      add rbx, 2; ++rbx
       push rcx
       push rbx
-      call prePrint
-      mov ax, 32
-      mov [toPrint], ax
+      call printBigNum
+      mov ax, 32; char(32) = ' '
       call print
       pop rbx
       pop rcx
       loop fullPrint
-    mov ax, 10
-    mov [toPrint], ax
+    mov ax, 10; char(10) = '\n'
     call print
     ret
 
@@ -55,9 +63,9 @@ section .text
     mov rbx, arr2
     call printTemplate
 
-    mov eax, 1
-    mov ebx, 0
-    int 0x80
+    mov eax, 1; sys call for sys_exit
+    mov ebx, 0; exit status
+    int 0x80; call kernel
 
 section .bss
   toPrint resb 1
