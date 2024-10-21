198afb60d6591440f229948a42d845f2477cf5d2
User1235321 <pavreb1235321@gmail.com>
2024-10-21 19:10:12 +0300

    Some corrects fourth CA lab

--- Sem3/Computer acrhitector/Lab4/1.asm
+++ Sem3/Computer acrhitector/Lab4/1.asm
@@ -3,12 +3,16 @@ section .data
 section .text
   _start:
 
-    add eax, ebx
-    add eax, [ebx]
-    add eax, [ebx + esi]
-    add eax,[ebx + esi + 12]
+    mov rbx, dopNum		;move rbx, to avoid a segment error
+
+    add rax, rbx
+    add rax, [rbx]
+    add rax, [rbx + rsi]
+    add rax, [rbx + rsi + 12]
+
     mov eax, 1
     mov ebx, 0
     int 0x80
 
 section .bss
+  dopNum resb 1
--- Sem3/Computer acrhitector/Lab4/main.cpp
+++ Sem3/Computer acrhitector/Lab4/main.cpp
@@ -1,51 +1,71 @@
+/*
+  I can't write universal function, which use assembler insertion with different commands.
+
+  Example:
+  void myFunction(std::string str)
+  {
+    __asm__(str);
+  }
+  int main()
+  {
+    std::string str = "addl %ebx, %eax\n\t"
+    myFunction(str)
+  }
+  Or
+  int main()
+  {
+    myFunction("addl %ebx, %eax\n\t")
+  }
+
+  So, i know, that macros is very bad, but in this situation i haven't choice.
+  P.s. don't burn my curtains please
+*/
 #include <iostream>
 #include <chrono>
-#include <string>
+
+#define mainFunction(value) \
+  start = timer1.now(); \
+  for (size_t i = 0; i < 100; ++i) \
+  { \
+    __asm__ \
+    ( \
+      value \
+      value \
+      value \
+      value \
+      value \
+      value \
+      value \
+      value \
+      value \
+      value \
+    ); \
+  } \
+  end = timer1.now(); \
+  std::cout << std::chrono::duration_cast< std::chrono::microseconds >(end - start).count() << " microseconds\n";
 
 int main()
 {
-  std::cout << "add ax, bx 10 x 100 times: ";
   std::chrono::steady_clock timer1;
   auto start = timer1.now();
-  for (size_t i = 0; i < 100; ++i)
-  {
-    asm
-    (
-      "add %ax, %bx \n\t"
-      "add %ax, %bx \n\t"
-      "add %ax, %bx \n\t"
-      "add %ax, %bx \n\t"
-      "add %ax, %bx \n\t"
-      "add %ax, %bx \n\t"
-      "add %ax, %bx \n\t"
-      "add %ax, %bx \n\t"
-      "add %ax, %bx \n\t"
-      "add %ax, %bx \n\t"
-    );
-  }
   auto end = timer1.now();
-  std::cout << std::chrono::duration_cast< std::chrono::microseconds >(end - start).count() << " microseconds\n";
 
-  std::cout << "add ax, [bx] 10 x 100 times: ";
-  start = timer1.now();
-  for (size_t i = 0; i < 100; ++i)
-  {
-    asm
-    (
-      "add %ax, %[bx] \n\t"
-      "add %ax, %[bx] \n\t"
-      "add %ax, %[bx] \n\t"
-      "add %ax, %[bx] \n\t"
-      "add %ax, %[bx] \n\t"
-      "add %ax, %[bx] \n\t"
-      "add %ax, %[bx] \n\t"
-      "add %ax, %[bx] \n\t"
-      "add %ax, %[bx] \n\t"
-      "add %ax, %[bx] \n\t"
-    );
-  }
-  end = timer1.now();
-  std::cout << std::chrono::duration_cast< std::chrono::microseconds >(end - start).count() << " microseconds\n";
+  std::cout << "add eax, 125 (addl $125, %%eax) 10 x 100 times: ";
+  mainFunction("addl $125, %eax\n\t");
 
+  std::cout << "add eax, ebx (addl %%ebx, %%eax) 10 x 100 times: ";
+  mainFunction("addl %ebx, %eax\n\t");
+
+  std::cout << "add eax, [ebx] (addl (%%ebx), %%eax) 10 x 100 times: ";
+  int * some = new int;
+  *some = 15;
+  __asm__
+  (
+    "movl %0, %%ebx\n\t"
+    :"=m"(some)
+  ); //if i just try to use "addl (%ebx), %eax", without change ebx, programm is crash
+  mainFunction("addl (%ebx), %eax\n\t");
+
+  delete some;
   return 0;
 }
