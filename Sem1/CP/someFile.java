2355f04b5a080de8731ff3997b1d11d19bf6c501
User1235321 <pavreb1235321@gmail.com>
2024-11-26 15:54:42 +0300

    Delete CP folder, because it's trash: just some random java files

--- Sem1/CP/someFile.java
+++
@@ -1,8 +0,0 @@
-import swingtest.TestWindow;
-
-public class someFile {
-  private swingtest.TestWindow someWindow = new swingtest.TestWindow("Some window");
-
-  public static void main(String[] args) {
-  }
-}
