46cd8fa64f6f95cad9a6ad4bde0967d6cc64c109
User1235321 <pavreb1235321@gmail.com>
2025-02-18 12:23:51 +0300

    Finish version of first lab of Microelectronics

--- Sem4/Microelectronics/Labs/Lab1/second.vhd
+++ Sem4/Microelectronics/Labs/Lab1/second.vhd
@@ -1,6 +1,5 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
 
 entity second is
   port
@@ -20,7 +19,6 @@ end second;
 architecture behav of second is
 type array_type is array (7 downto 0) of std_logic_vector(7 downto 0);
 signal dffArr : array_type;
---signal num : integer range 0 to 8 := 0;
 signal over : std_logic;
 
 
@@ -28,40 +26,38 @@ begin
   process(CLK)
   variable num: integer;
   begin
-    if ((CLK'event) and (CLK = '1')) then
-      --num <= num + 1;
-      num:= num + 1;
-      if (num = 8) then
-        over <= '0';
-        --num <= 1;
-        num := 1;
-      end if;
-
-      --compare
-      if (KKS = dffArr(7)) then
-        --num <= 1;
-        num := 1;
-        Synchro <= '1';
-      else
-        Synchro <= '0';
-      end if;
-
+    if (rising_edge(CLK)) then
       --dff block
       for i in 7 downto 1 loop
         dffArr(i) <= dffArr(i - 1);
       end loop;
 
-      if (((Query = '1') and (dffArr(7) = KKS)) or (over = '1')) then
-        if (over = '0') then
-          over <= '1';
+      if (((Query = '1') and (dffArr(7) = KKS)) or (over = '0')) then
+        if (over = '1') then
+          over <= '0';
         end if;
         dffArr(0) <= DATA;
       else
         dffArr(0) <= dffArr(7);
       end if;
+
+      num := num + 1;
+      if (num = 7) then
+        over <= '1';
+        num := 0;
+      end if;
+
+      --compare
+      if (KKS = dffArr(7)) then
+        num := 0;
+        Synchro <= '1';
+      else
+        Synchro <= '0';
+      end if;
     end if;
+
+    Get <= not over;
+    Q <= dffArr(7);
+    qq <= num;
   end process;
-  Get <= over;
-  Q <= dffArr(7);
-  --qq <= num;
 end behav;
