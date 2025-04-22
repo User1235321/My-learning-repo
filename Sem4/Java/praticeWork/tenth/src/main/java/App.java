9ccd9cd88ed3deee0da05cd5fa1b2524f202e4bb
User1235321 <pavreb1235321@gmail.com>
2025-04-22 17:23:29 +0300

    Add tenth practice work on Java

--- Sem4/Java/praticeWork/tenth/src/main/java/App.java
+++ Sem4/Java/praticeWork/tenth/src/main/java/App.java
@@ -1,39 +1,51 @@
 package tenth;
 
 import java.lang.Thread;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.concurrent.*;
+import java.util.Random;
+import java.util.concurrent.atomic.*;
 
 public class App
 {
+
+  static ConcurrentHashMap< String, AtomicInteger > pageMap = new ConcurrentHashMap<>();
+  private static void visitPage(String pageName)
+  {
+    if (pageMap.containsKey(pageName))
+    {
+      pageMap.get(pageName).incrementAndGet();
+    }
+    else
+    {
+      System.out.println("Can't find page: " + pageName);
+    }
+  }
   public static void main(String[] args)
   {
-    String fileName = "nFile";
-    int num = 0;
-    File file = new File(fileName);
-    try
+    var executor = Executors.newFixedThreadPool(10);
+    String [] pageNames = new String[5];
+    for (int i = 0; i < pageNames.length; ++i)
     {
-      Scanner scanner = new Scanner(file);
-      num = scanner.nextInt();
-      scanner.close();
-    }
-    catch (FileNotFoundException e)
-    {
-      System.out.println("App.java:main:string 22 can't read number from file: " + fileName);
+      Random random = new Random();
+      pageNames[i] = "page" + random.nextInt(pageNames.length * 100);
+      pageMap.put(pageNames[i], new AtomicInteger());
     }
 
-    Materials material = new Materials();
-    WorkProcess workProcess = new WorkProcess();
-
-    CountDownLatch latch = new CountDownLatch(1);
-    Thread customer = new Thread(new Customer(material, workProcess, latch));
-    Thread workers[] = new Thread[num];
-    for (int i = 0; i < num; ++i)
+    try (executor)
     {
-      workers[i] = new Thread(new Worker(material, workProcess, latch));
-      workers[i].start();
+      for (int i = 0; i < 1000; ++i)
+      {
+        executor.execute(() -> {
+          Random random = new Random();
+            visitPage(pageNames[random.nextInt(pageNames.length)]);
+          }
+        );
+      }
     }
-    customer.start();
+
+    for (int i = 0; i < pageNames.length; ++i)
+    {
+      System.out.println("Page \"" + pageNames[i] + "\" visited " + pageMap.get(pageNames[i]) + " times");
+    }
   }
 }
